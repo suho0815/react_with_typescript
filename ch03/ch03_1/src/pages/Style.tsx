d5b90e4c035b17207155f5e9736a12a61fa17f36
suho0815 <50311505+suho0815@users.noreply.github.com>
2023-07-10 04:44:34 +0900

    ch03_1

---
+++ ch03/ch03_1/src/pages/Style.tsx
@@ -0,0 +1,13 @@
+export default function Style() {
+  return (
+    <div>
+      <h3>Style</h3>
+      <span className="material-icons" style={{color: 'blue'}}>
+        home
+      </span>
+      <span className="material-icons" style={{color: 'red', fontSize: '50px'}}>
+        check_circle_outline
+      </span>
+    </div>
+  )
+}

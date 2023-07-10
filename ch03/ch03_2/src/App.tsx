309e1548a18224368ce1a88e03e4ee9da6d4f52d
suho <suho0815@naver.com>
2023-07-10 13:57:01 +0900

    ch03_3 ~ing

---
+++ ch03/ch03_2/src/App.tsx
@@ -0,0 +1,14 @@
+import Tailwindcss from './pages/Tailwindcss'
+import Color from './pages/Color'
+import TextsTest from './pages/TextsTest'
+import './App.css'
+
+export default function App() {
+  return (
+    <div>
+      <Tailwindcss/>
+      <Color/>
+      <TextsTest/>
+    </div>
+  )
+}
---
+++ ch03/ch03_2/src/pages/TextsTest.tsx
@@ -0,0 +1,14 @@
+import * as D from '../data'
+import {Title, Subtitle, Summary, Paragraph} from '../components'
+
+export default function TextsTest() {
+    return (
+        <div>
+            <Title>TextsTest</Title>
+            <div>
+                
+
+            </div>
+        </div>
+    )
+}

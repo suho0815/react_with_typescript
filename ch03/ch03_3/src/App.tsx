246398264cc24c47026ea66aa703e8d8ba44d3ec
suho0815 <50311505+suho0815@users.noreply.github.com>
2023-07-11 04:45:31 +0900

    ch03_3 ~ing

---
+++ ch03/ch03_3/src/App.tsx
@@ -0,0 +1,32 @@
+import DivTest from './pages/DivTest'
+import ViewportTest from './pages/ViewportTest'
+import HeightTest from './pages/HeightTest'
+import PaddingTest from './pages/PaddingTest'
+import MarginTest from './pages/MarginTest'
+import ImageTest from './pages/ImageTest'
+import BackgroundImageTest from './pages/BackgroundImageTest'
+import DisplayTest from './pages/DisplayTest'
+import DisplayNoneTest from './pages/DisplayNoneTest'
+import AvatarTest from './pages/AvatarTest'
+import PositionTest from './pages/PaddingTest'
+import OverlayTest from './pages/OverlayTest'
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+      <OverlayTest />
+      <PositionTest />
+      <DisplayNoneTest />
+      <DisplayTest />
+      <AvatarTest />
+      <BackgroundImageTest />
+      <ImageTest />
+      <MarginTest />
+      <PaddingTest />
+      <HeightTest />
+      <ViewportTest />
+      <DivTest />
+    </main>
+  )
+}
---
+++ ch03/ch03_3/src/pages/PaddingTest.tsx
@@ -0,0 +1,10 @@
+import {Title} from '../components'
+
+export default function PaddingTest() {
+  return (
+    <section className="mt-4">
+      <Title>PaddingTest</Title>
+      <div className="p-8"></div>
+    </section>
+  )
+}

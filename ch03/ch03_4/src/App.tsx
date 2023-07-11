b6671e507eb104bf2759a209d46115cd3b2eb47c
suho <suho0815@naver.com>
2023-07-11 17:56:31 +0900

    ch03_4 ~ ing

---
+++ ch03/ch03_4/src/App.tsx
@@ -0,0 +1,22 @@
+import DirectionTest from './pages/DirectionTest'
+import WrapTest from './pages/WrapTest'
+import MinMaxTest from './pages/MinMaxTest'
+import JustifyCenterTest from './pages/JustifyCenterTest'
+import AlignTest from './pages/AlignTest'
+import UserContainer from './pages/UserContainer'
+import CardContainer from './pages/CardContainer'
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+      <CardContainer/>
+      <UserContainer/>
+      <AlignTest/>
+      <JustifyCenterTest/>
+      <MinMaxTest/>
+      <WrapTest/>
+      <DirectionTest/>
+    </main>
+  )
+}

722e189aea4bf3b7208bba6085a1426bd8fef6f4
suho <suho0815@naver.com>
2023-07-14 13:50:49 +0900

    ch04~~

---
+++ ch04/ch04_1/src/App.tsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react'
+import Clock from './pages/Clock'
+
+export default function App() {
+  let today = new Date()
+  
+  useEffect(() => {
+    console.log('useEffect called.')
+    const duration = 1000
+    const id = setInterval(()=> {
+    today = new Date()
+    console.log('today', today.toLocaleTimeString())
+  }, duration)
+  return () => clearInterval(id)
+  },[])
+
+  return (
+    <Clock today={today}/>
+  )
+}
